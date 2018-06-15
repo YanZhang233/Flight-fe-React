91bc58cb71061923440d81e0420ecfccaa60ae8b
Yan <zyellie7@gmail.com>
2018-06-15 10:44:48 -0400

    Update display UI.

--- src/components/DisplayRequest.js
+++ src/components/DisplayRequest.js
@@ -1,6 +1,6 @@
 import React from "react";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
-import { faFighterJet } from "@fortawesome/fontawesome-free-solid";
+import { faUsers } from "@fortawesome/fontawesome-free-solid";
 
 class DisplayRequest extends React.Component {
 
@@ -26,17 +26,34 @@ class DisplayRequest extends React.Component {
                     Airport
                     <span className="right">{airport}</span>
                 </p>
-                <p>Destination: {destination}</p>
-                <p>Time: {time}</p>
-                <p>Flight Information: {flightInfo}</p>
-                <p>Number Of People: {numOfPeople}</p>
-                <p>Baggage: {baggage}</p>
-                <p>{description}</p>
+                <p className="displayRow">
+                    Destination
+                    <span className="right">{destination}</span>
+                </p>
+                <p className="displayRow">
+                    Time
+                    <span className="right">{time}</span>
+                </p>
+                <p className="displayRow">
+                    Flight Information
+                    <span className="right">{flightInfo}</span>
+                </p>
+                <p className="displayRow">
+                    Number Of People
+                    <span className="right">{numOfPeople}</span>
+                </p>
+                <p className="displayRow">
+                    Baggage
+                    <span className="right">{baggage}</span>
+                </p>
+                <p className="displayRow">
+                    {description}
+                </p>
                 <button className="likeNum"
                     disabled={noLike}
                     onClick={this.props.likeSwitch}
                 >
-                    {noLike ? "No one wants to pick you up :)" : "There are " + like + " people want to pick you up."}
+                    <FontAwesomeIcon icon={faUsers} /> {noLike ? "No one wants to pick you up :)" : "There are " + like + " people want to pick you up."}
                 </button>
             <button onClick={this.props.editSwitch}>Edit</button>
           </li>
