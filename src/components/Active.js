13bcb69413737130f1a9c8f8eecee9c6e1ddb58d
Yan <zyellie7@gmail.com>
2018-06-19 15:02:27 -0400

    Add timer in active.

--- src/components/Active.js
+++ src/components/Active.js
@@ -3,8 +3,18 @@ import { withRouter } from "react-router-dom";
 
 class Active extends React.Component {
 
+    state = {
+        count: 5
+    }
+
     componentDidMount() {
         setTimeout(() => this.props.history.push(`/`), 5000);
+        setInterval(this.timer, 1000);
+    }
+
+    timer = () => {
+        const time = this.state.count - 1;
+        this.setState({ count: time });
     }
 
     handleClick = () => {
@@ -14,7 +24,7 @@ class Active extends React.Component {
     render () {
         return (
             <div className="active">
-                <p>Active your email successfully! Back to system 5 seconds later...</p>
+                <p>Active your email successfully! Back to system {this.state.count} seconds later...</p>
                 <br/>
                 <button className="btn btn-primary" onClick={this.handleClick}>Go To Flight</button>
             </div>
