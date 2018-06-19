2eeb1c1a043b857fd530c84ade97610e1423a83c
Yan <zyellie7@gmail.com>
2018-06-19 14:04:25 -0400

    Update Intro.

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -40,19 +40,36 @@ class Intro extends React.Component {
                    <div className="container">
                        <div className="jumbotron">
                            <h1>Welcome</h1>
-                           <p>blablabla</p>
+                           <p className="welcome">This is a platform providing pick-up request information for the new students who are seeking for pick-up service at airport, and those people who want to give a help. </p>
                            <p><a className="btn btn-primary btn-lg" href="/login">Get Started</a></p>
                        </div>
-                   </div>
+                       <div className="col-xs-12 col-md-5">
+                           <h3 className="entry-title">使用说明</h3>
+                           <div className="line"></div>
+                           <span className="post"> <i className="fas fa-calendar-alt"></i> 2018.06.15 by zz</span>
+                           <p className="para">新生申请接机: 请点击注册，填写个人信息(如家乡，毕业院校等)，登陆后请详细填写接机请求(如到达时间，航班信息等)，申请完成后，会进行匹配。接机人会根据接机信息选择接送需要接机的新同学
+                           ,匹配完成后会将微信号邮箱等联系方式发送给新同学。</p>
+                           <p className="para">接机人申请: 注册时选择提供帮助，只支持edu邮箱注册，注册完成后登录即可浏览所有的接机请求，和请求对应的新生个人信息；可选择提供帮助，点击提供帮助后会将您的联系方式发送给新生。</p>
+                       </div>
 
-                   <div className="container">
-                       <div className="entry">
+                       <div className="col-xs-0 col-xs-2"></div>
+
+                       <div className="col-xs-12 col-md-5">
                            <h3 className="entry-title">Intro to this web</h3>
                            <div className="line"></div>
                            <span className="post"> <i className="fas fa-calendar-alt"></i> 2018.06.15 by zz</span>
-                           <p className="para">This is a website that helps you ...</p>
+                           <p className="para">For new student: sign up as new student, fill in personal information(hometown, school,etc.),
+                               login to fill in the form about your request information(arrival time, flight number,etc.). After submiting the form, senior students will select the new students who need help by their request info.</p>
+                           <p className="para">For senior students: sign up as senior student, only edu emails are supported, after registration, you can login to
+                            view all pick-up requests and the personal info about the corresponding new student; You can choose to provide help. After clicking help, your contact info will be sent to
+                            the new student.</p>
                        </div>
+
                    </div>
+
+                   <p className="copyright">
+                       2018-06
+                   </p>
 
 
                </ReactCSSTransitionGroup>
