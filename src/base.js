911e381dbee725acf7369823fdc3f320995122de
Yan <zyellie7@gmail.com>
2018-06-20 14:33:20 -0400

    Update baseURL.

--- src/base.js
+++ src/base.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: `http://flight.zhuo9529.com`,
-    //baseURL: `https://flight.foggystudio.com`,
+    //baseURL: `http://flight.zhuo9529.com`,
+    baseURL: `https://flight.foggystudio.com`,
     //baseURL: `http://localhost:8080`,
     withCredentials: true
 });
